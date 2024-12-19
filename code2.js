b733df89d22075072c0e82901b5d04994480482a
ruteshchavan <ruteshrc007@gmail.com>
2024-12-19 12:11:43 +0530

    add

--- code2.js
+++ code2.js
@@ -1,4 +1,5 @@
 // guess the number
+// create a game where user enters a random number. Ask user to guess the game number until user enters the correct value.
 
 let getnumber=25;
 let usernumber=prompt("Guess the number");
---
+++ for-of.js
@@ -0,0 +1,5 @@
+let a ="JavaScript";
+
+for(let i of a){
+  console.log("i = ",i);  // iterates over the character 
+}   
