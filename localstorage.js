868558ec2932d8db48b1f37fbd4ce7be88e1fe86
ruteshchavan <ruteshrc007@gmail.com>
2025-01-22 09:54:58 +0530

    add

---
+++ localstorage.js
@@ -0,0 +1,15 @@
+// localstorage is browsers internal storage.
+// only supports and store string data type.
+
+
+localStorage.setItem('fname','faceless');   // stores data as key value pairs
+localStorage.setItem('lname','void');
+
+
+
+
+
+console.log(localStorage.getItem('fname'));  // getItem is used to get the data 
+console.log(localStorage.getItem('lname'));
+
+// localStorage.clear() is used to clear the localStorage DataTransfer.
--- object.js
+++ object.js
@@ -2,8 +2,17 @@ let student= {
     name:"Rutesh chavan",
     age:22,
     college:'DYPCET',
+    message(){
+        console.log(`Welcome to ${student.college}`);
+    }
 };
 console.log(student.name);
+student.message();
+
+//de-structuring
+let{name,age,college}=student;
+console.log(name);
+console.log(college);
 
 /* 
 object is collection of variables
@@ -11,6 +20,8 @@ to access only particular value form student object - object[key] ( student["nam
 to update any key - student[name]= "raja singh";
                     student[age] = student[age] + 1
                     note -  constant object key can be changed but not constant variable.
+
+Autoboxing : automatic conversion of primitives to object.  
 
 
 */
