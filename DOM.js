6974061247ea599bb0d56e7e357894cf4b833331
ruteshchavan <ruteshrc007@gmail.com>
2025-01-22 09:54:31 +0530

    add

---
+++ DOM.js
@@ -0,0 +1,17 @@
+console.log(document.location)    // returns the file location of file , url etc..
+
+console.log(document.title)    // returns title of page
+// document.title="Hello";   // changes the title of document
+
+console.log(document.domain)   // returns the domain
+
+console.log(document.querySelector('h1'));
+
+console.log(document.querySelector('button').classList);  // reurns the classname 
+// console.log(document.querySelector('button').classList.add('btn2'));  // add another class
+console.log(document.querySelector('button').classList.remove('btn'));  // removes the class
+
+let a=document.createElement('h2');   // create new element 
+console.log(a);
+
+
