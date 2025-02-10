b5323f90201f44c3cb9cb73ac8add2e00815c7ab
ruteshchavan <ruteshrc007@gmail.com>
2025-02-10 18:42:36 +0530

    add

--- event.js
+++ event.js
@@ -1,2 +1,9 @@
 // the change in the state of an object is known as event.
 
+
+let a=document.querySelector("#a1");
+
+// both the buttons are been clicked..
+a.onclick= () =>{
+  console.log('hello');
+}
