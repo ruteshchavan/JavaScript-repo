ec2c4f6e91dd9e1abf4fb0e515280e53f26185eb
ruteshchavan <ruteshrc007@gmail.com>
2025-02-10 18:30:34 +0530

    add

---
+++ domm.js
@@ -0,0 +1,8 @@
+// returns the element by tag name.
+let a=document.getElementById("a1");
+console.log(a);
+
+// returns the element by class name.
+let b=document.getElementsByClassName("b1");
+console.log(b);
+
